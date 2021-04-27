8281586349b9f87c3de44d87fdbce168344d6c80
KhanhND <khanhnd162@gmail.com>
2021-04-27 11:39:32 +0700

    Add dev branch

--- convert.py
+++ convert.py
@@ -1,10 +1,11 @@
-import cv2
-import logging
-from pathlib import Path
+import glob
+import os
 from os import listdir
 from os.path import join, isfile
+from pathlib import Path
 
-logger = logging.getLogger(__name__)
+import cv2
+from PIL import Image
 
 
 def process_directory(directory):
@@ -17,14 +18,38 @@ def process_directory(directory):
 
 
 def convert_png_to_jpg_dir(directory):
-    list_file = process_directory(directory)
-    for file_path in list_file:
-        convert_png_to_jpg(file_path)
+    files = glob.glob(f"{directory}/*")
+
+    for file in files:
+        convert_png_to_jpg(file)
 
 
 def convert_png_to_jpg(file_path):
-    logger.info(file_path)
+    print(file_path)
+
     image = cv2.imread(file_path)
     file_name = Path(file_path).stem
-    cv2.imwrite('{}.jpg'.format(file_name), image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    cv2.imshow('Display Image', image)
+    cv2.waitKey(0)
+    # transform image to grayscale
+    image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    # apply cv2.THRESH_TRUNC or cv2.THRESH_OTSU
+    _, image_new = cv2.threshold(image_gray, 127, 255, cv2.THRESH_TRUNC or cv2.THRESH_OTSU)
+    cv2.imwrite(f"{'data/train/{}.png'.format(file_name)}", image_new)
+
+    image = Image.open(f"data/train/{'{}.png'.format(file_name)}")
+    image = image.convert("P")
+    image2 = Image.new("P", image.size, 255)
+
+    # set pixel black
+    for x in range(image.size[1]):
+        for y in range(image.size[0]):
+            pixel = image.getpixel((y, x))
+            if pixel < 115:
+                image2.putpixel((y, x), 0)
+
+    # save converted image
+    image2.save(f"data/train/{os.path.basename(file_path)}")
+    # glob.delete(f"{path_finish}/aplicado.png")
 
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # -*- coding:utf-8 -*-
 import logging
-from PIL import Image
-import cv2
 from os import listdir
 from os.path import join, isfile
-import convert
+
+import cv2
+from PIL import Image
 
 # Create a custom logger
-import download
 
 logger = logging.getLogger(__name__)
 
 RAW_PATH = "data/raw/"
+TRAIN_PATH = "data/train/"
 SLICED_PATH = "data/sliced/"
 
 chars_list = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -47,4 +47,8 @@ def reduce_noise_dir(directory):
 if __name__ == "__main__":
     # download.download_captcha_raw()
     # reduce_noise_dir(RAW_PATH)
-    convert.convert_png_to_jpg_dir(RAW_PATH)
+    # convert.convert_png_to_jpg_dir(RAW_PATH)
+    print(cv2.__version__)
+    image = cv2.imread('data/raw/0.png', cv2.IMREAD_UNCHANGED)
+    cv2.imshow('Display Image', image)
+    cv2.waitKey(0)
